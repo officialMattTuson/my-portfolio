9fb87ee180b88e630d5eb2d22a8e1f3a43bf89d4
cornrowsMatt <mtuson1414@gmail.com>
2023-08-30 20:57:06 +1200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Portfolio';
+
+  constructor(private elementRef: ElementRef) {}
+
+  ngAfterViewInit() {
+    this.initializeObserver();
+  }
+
+  handleIntersection(entries: IntersectionObserverEntry[], observer: IntersectionObserver) {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('in-view');
+      } else {
+        entry.target.classList.remove('in-view');
+      }
+    });
+  }
+
+  initializeObserver() {
+
+    const options: IntersectionObserverInit = {
+      root: null,
+      rootMargin: '-100px',
+      threshold: 0.3,
+    };
+
+    const observer = new IntersectionObserver(this.handleIntersection, options);
+
+    const sectionsToObserve: NodeListOf<Element> = this.elementRef.nativeElement.querySelectorAll('.hidden');
+    sectionsToObserve.forEach(section => observer.observe(section));
+  }
+  
 }
